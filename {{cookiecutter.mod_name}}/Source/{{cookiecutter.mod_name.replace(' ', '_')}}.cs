a83787e428366c843ae3355891c803bd1ef61bc5
Alex Tearse-Doyle <alextearsedoyle@gmail.com>
2018-03-07 16:48:34 -0800

    harmony, settings, DebugLog

--- {{cookiecutter.mod_name}}/Source/{{cookiecutter.mod_name.replace(' ', '_')}}.cs
+++ {{cookiecutter.mod_name}}/Source/{{cookiecutter.mod_name.replace(' ', '_')}}.cs
@@ -1,6 +1,32 @@
-﻿namespace {{cookiecutter.mod_name.replace(' ', '_')}}
+﻿using System.Reflection;
+using Verse;
+using UnityEngine;
+using Harmony;
+
+namespace {{cookiecutter.mod_name.replace(' ', '_')}}
 {
-    public class {{cookiecutter.mod_name.replace(' ', '_')}}
-    {
-    }
+	public class Mod : Verse.Mod
+	{
+		public Mod(ModContentPack content) : base(content)
+		{
+			// initialize settings
+			GetSettings<Settings>();
+#if DEBUG
+			HarmonyInstance.DEBUG = true;
+#endif
+			HarmonyInstance harmony = HarmonyInstance.Create("uuugggg.rimworld.{{cookiecutter.mod_name.replace(' ', '_')}}.main");
+			harmony.PatchAll(Assembly.GetExecutingAssembly());
+		}
+
+		public override void DoSettingsWindowContents(Rect inRect)
+		{
+			base.DoSettingsWindowContents(inRect);
+			GetSettings<Settings>().DoWindowContents(inRect);
+		}
+
+		public override string SettingsCategory()
+		{
+			return "{{cookiecutter.mod_name}}";
+		}
+	}
 }
