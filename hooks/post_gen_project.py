5b7484356a576c62c612160f715a11d0eedc62af
Pierce Corcoran <pierce@plasticcow.com>
2020-02-25 16:44:36 -0800

    Update to RimWorld 1.1

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,8 +1,8 @@
 import os
 
 #About is always made
-os.makedirs("{{cookiecutter.mod_name}}/Defs", exist_ok=True)
-os.makedirs("{{cookiecutter.mod_name}}/Patches", exist_ok=True)
-os.makedirs("{{cookiecutter.mod_name}}/Languages", exist_ok=True)
-os.makedirs("{{cookiecutter.mod_name}}/Sounds", exist_ok=True)
-os.makedirs("{{cookiecutter.mod_name}}/Textures", exist_ok=True)
+os.makedirs("Dev/Defs", exist_ok=True)
+os.makedirs("Dev/Patches", exist_ok=True)
+os.makedirs("Dev/Languages", exist_ok=True)
+os.makedirs("Dev/Sounds", exist_ok=True)
+os.makedirs("Dev/Textures", exist_ok=True)
--- {{cookiecutter.mod_name}}/Dev/Source/{{cookiecutter.mod_name.replace(' ', '_')}}.cs
+++ {{cookiecutter.mod_name}}/Dev/Source/{{cookiecutter.mod_name.replace(' ', '_')}}.cs
@@ -3,7 +3,7 @@ using System.Linq;
 using Verse;
 using RimWorld;
 using UnityEngine;
-{%if(cookiecutter.harmony != 'n')%}using Harmony;
+{%if(cookiecutter.harmony != 'n')%}using HarmonyLib;
 {%endif%}
 namespace {{cookiecutter.mod_name.replace(' ', '_')}}
 {
@@ -11,16 +11,14 @@ namespace {{cookiecutter.mod_name.replace(' ', '_')}}
     {
         public Mod(ModContentPack content) : base(content)
         {
-{%if(cookiecutter.helloWorld != 'n')%}          Log.Message("Hello world from {{cookiecutter.mod_name}}");
-
-{% endif %}{%if(cookiecutter.settings != 'n')%}         // initialize settings
+{%if(cookiecutter.settings != 'n')%}         // initialize settings
             GetSettings<Settings>();
 
 {%endif%}{%if(cookiecutter.harmony != 'n')%}#if DEBUG
-            HarmonyInstance.DEBUG = true;
+            Harmony.DEBUG = true;
 #endif
 
-            HarmonyInstance harmony = HarmonyInstance.Create("{{cookiecutter.author}}.rimworld.{{cookiecutter.mod_name.replace(' ', '_')}}.main");
+            Harmony harmony = new Harmony("{{cookiecutter.author}}.rimworld.{{cookiecutter.mod_name.replace(' ', '_')}}.main");
 {%if(cookiecutter.defOfWarn != 'n')%}
             //Turn off DefOf warning since harmony patches trigger it.
             MethodInfo DefOfHelperInfo = AccessTools.Method(typeof(DefOfHelper), "EnsureInitializedInCtor");
